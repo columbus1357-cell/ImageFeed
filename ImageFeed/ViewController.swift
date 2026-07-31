3fa14c0885186b56b9d780c3382ad4c7cc29c9d7
Ymir <Columbus1357@gmail.com>
2026-07-31 16:48:58 +0700

    feat: updated gitignore and viewcontroller

--- ImageFeed/ViewController.swift
+++ ImageFeed/ViewController.swift
@@ -5,14 +5,14 @@
 //  Created by Aleksandr on 30.07.2026.
 //
 
-import UIKit
+import UIKit 
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+    } 
 
 
 }
