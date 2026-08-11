0790f831cf339138d11cc10e68fde5b8512b4ee3
Ymir <Columbus1357@gmail.com>
2026-08-11 20:18:46 +0700

    Сохранил реализацию перехода к картинке через Navigation Controller и Push Segue

---
+++ ImageFeed/ImageFeed/SingleImageViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Aleksandr on 08.08.2026.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    var image: UIImage? {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+        }
+    }
+    
+    @IBOutlet private weak var imageView: UIImageView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView.image = image
+        
+    }
+    
+
+   
+}
--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -11,6 +11,7 @@ final class ImagesListViewController: UIViewController {
     @IBOutlet private var tableView: UITableView!
     
     private let photosName: [String] = Array(0..<20).map{ "\($0)" }
+    private let showSingleImageSegueIdentifier = "ShowSingleImage"
     
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
@@ -36,6 +37,23 @@ final class ImagesListViewController: UIViewController {
         let likeImage = UIImage(resource: isLiked ? .likeActive : .likeNoActive)
         cell.likeButton.setImage(likeImage, for: .normal)
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showSingleImageSegueIdentifier {
+            guard
+                let viewController = segue.destination as? SingleImageViewController,
+                let indexPath = sender as? IndexPath
+            else {
+                assertionFailure("Invalid segue destination")
+                return
+            }
+
+            let image = UIImage(named: photosName[indexPath.row])
+            viewController.image = image
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
+    }
 }
 
 extension ImagesListViewController: UITableViewDataSource {
@@ -57,7 +75,9 @@ extension ImagesListViewController: UITableViewDataSource {
 
 extension ImagesListViewController: UITableViewDelegate {
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: showSingleImageSegueIdentifier, sender: indexPath)
+    }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         let imageName = photosName[indexPath.row]
