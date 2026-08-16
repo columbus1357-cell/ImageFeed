a5c57002ea2bdda9b34a400cc0535510a77d824f
Ymir <Columbus1357@gmail.com>
2026-08-16 17:46:16 +0700

    Добавлен файл Constants с ключами API Unsplash

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Aleksandr on 16.08.2026.
+//
+
+import Foundation
+
+enum Constants {
+    static let accessKey: String = "gFz1wn2fQERVRZhp4GOdlUguyAvFlB54tv1CsoASFT4"
+    static let secretKey: String = "BCA8Zsiv_Xx-q2WU-Mt2uhFtqayxwXR_yTbzCWLMmCw"
+    static let redirectURI: String = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope: String = "public+read_user+write_likes"
+    static let defaultBaseURLString: String = "https://api.unsplash.com"
+}
+
